de057d143fa22066b504a960b92b65bbc4db95c3
Dmitriy Filatov <filatov-d@mail.ru>
2022-08-31 12:00:08 +0300

    Homework #2 updated

--- hw1_2/Program.cs
+++ hw1_2/Program.cs
@@ -4,11 +4,15 @@ int number_1 = int.Parse(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int number_2 = int.Parse(Console.ReadLine());
 
-if(number_1 > number_2)
+if (number_1 > number_2)
 {
     Console.WriteLine($"Число {number_1} больше чем число {number_2}");
 }
+else if (number_1 < number_2)
+{
+    Console.WriteLine($"Число {number_2} больше чем число {number_1}");
+}
 else
 {
-    Console.WriteLine($"Число {number_2} больше чем число {number_2}");
+    Console.WriteLine($"Число {number_1} равно числу {number_2}");
 }
