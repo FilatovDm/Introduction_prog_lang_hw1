7551800c1f2bc20e5a25254a18ebef6e5cbd4ef4
Dmitriy Filatov <filatov-d@mail.ru>
2022-08-31 11:45:14 +0300

    Homework #2 completed

---
+++ hw1_2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число: ");
+int number_1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int number_2 = int.Parse(Console.ReadLine());
+
+if(number_1 > number_2)
+{
+    Console.WriteLine($"Число {number_1} больше чем число {number_2}");
+}
+else
+{
+    Console.WriteLine($"Число {number_2} больше чем число {number_2}");
+}
